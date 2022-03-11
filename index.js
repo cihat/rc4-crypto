ff2cadbc01ea4497a75e01df013d2cc3f753856d
cihat <cihat02020202@gmail.com>
2022-03-11 14:12:49 +0300

    Update variable name && Add some comments

--- index.js
+++ index.js
@@ -9,29 +9,32 @@ const decrypt = (str, key = "rc4@123") => {
 const rc4 = (str, key) => {
   let s = [],
     j = 0,
-    x,
+    temp,
     result = ""
 
+  //! Key-scheduling algorithm (KSA)
   for (let i = 0; i < 256; i++) {
     s[i] = i
   }
-
   for (let i = 0; i < 256; i++) {
     j = (j + s[i] + key.charCodeAt(i % key.length)) % 256
-    x = s[i]
+    console.log("ASCII Code: ", j)
+    console.log("Char: ", String.fromCharCode(j))
+    temp = s[i]
     s[i] = s[j]
-    s[j] = x
+    s[j] = temp
   }
 
   let i = 0
   j = 0
 
+  //! Pseudo-random generation algorithm (PRGA)
   for (let y = 0; y < str.length; y++) {
     i = (i + 1) % 256
     j = (j + s[i]) % 256
-    x = s[i]
+    temp = s[i]
     s[i] = s[j]
-    s[j] = x
+    s[j] = temp
     result += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256])
   }
   return result
